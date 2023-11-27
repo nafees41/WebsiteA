c154cc23e78cadf38ff2fdb6a88825f7352c71b6
Nafees Rehman <nafeesurrehman5566@gmail.com>
2023-11-27 14:59:44 +0500

    updates

--- src/Components/FAQ/Faq.js
+++ src/Components/FAQ/Faq.js
@@ -41,29 +41,24 @@ const Faq = () => {
           </Typography>
         </Grid>
       </Grid>
-  
-      <Grid
+       <Grid
         container
         spacing={2}
         sx={{ marginTop: "20px", display: "flex", justifyContent: "center" ,paddingX:{lg:'0',md:'40px',sm:'40px',xs:'20px'}}} >
          <Grid item lg={8} md={12} sm={12} xs={12}>
          <div>
               {[
-                {
-                  label: "How we work at our brand?",
+               {
+                label: "How we work at our brand?",
                    content: (
                     <div>
                       <Typography   sx={{fontSize:'20px',fontWeight:'400'}}>
                       You offer a good product, make advertising, analyze the market<br/>but still have no improvement Our qulificated specialists 
                       </Typography>
-                     
-                    </div>
+                      </div>
                   ),
                 },
-                
-
-        
-              ].map((section, index) => (
+                 ].map((section, index) => (
                 <Accordion
                   key={index}
                   expanded={expanded[index] || false}
@@ -76,14 +71,11 @@ const Faq = () => {
               expanded[index] ? (
                 <RemoveIcon sx={{ color: 'white', backgroundColor: '#F7941D' }} />
               ) : (
-                <AddIcon sx={{ color: 'white', backgroundColor: '#F7941D' }} />
-              )
-            }>
-                  
-                    <Typography  variant="h6" sx={{fontWeight:'600',color:'#484444'}}>{section.label}</Typography>
-                    
-                  </AccordionSummary>
-                  <AccordionDetails  sx={{ fontWeight:'400',backgroundColor:'#FAFAFA',color:'#484444'}} >{section.content}</AccordionDetails>
+                <AddIcon sx={{ color: 'white', backgroundColor: '#F7941D' }}/>
+            )}>
+                 <Typography  variant="h6" sx={{fontWeight:'600',color:'#484444'}}>{section.label}</Typography>
+                      </AccordionSummary>
+                  <AccordionDetails  sx={{fontWeight:'400',backgroundColor:'#FAFAFA',color:'#484444'}}>{section.content}</AccordionDetails>
                 </Accordion>
               ))}
             </div>
