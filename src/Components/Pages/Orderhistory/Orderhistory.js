f14356b588782e6725bb10613a519d5d0524ffc6
Nafees Rehman <nafeesurrehman5566@gmail.com>
2023-12-02 14:27:49 +0500

    updates

--- src/Components/Pages/Orderhistory/Orderhistory.js
+++ src/Components/Pages/Orderhistory/Orderhistory.js
@@ -129,7 +129,7 @@ const Orderrecord = () => {
               sm={12}
               xs={12}
               sx={{border: "1px solid #D5D5D5",borderRadius:'15px'}} >
-           <Grid  container>
+           <Grid container>
             <Grid  item md={11} sm={12}  xs={12}>
               <Box sx={{display: "flex"}}>
               <Typography
